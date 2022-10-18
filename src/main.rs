a102bc15df503caacbeea3db4806f9fd955b8434
Simon Shamoon <simonshamoon@loop-249-237.loop-wireless.depaul.edu>
2022-10-18 11:56:56 -0500

    overrode print macros to make things look nicer

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
     loop {}
 }
